15b8084c43477994b1613f2ba6188827472520b3
JamesJaebi <jamesbenedicttenchavez@gmail.com>
2024-11-13 10:09:15 +0800

    Add files via upload

---
+++ LabExerc.java
@@ -0,0 +1,21 @@
+package labexerc;
+
+public class LabExerc {
+
+    public static void main(String[] args) {
+        int iNumber = 3;
+
+        if (iNumber >= 0) {
+
+            System.out.println("First String");
+        }
+
+        if (iNumber == 0) {
+            System.out.println("Second String");
+        } else {
+            System.out.println("Third String");
+        }
+
+    }
+
+}
